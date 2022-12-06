e0ab47a458b4027e7f494da87d20c3f5abc63d9d
Nuri <62878148+202110889@users.noreply.github.com>
2022-12-06 21:27:56 +0900

    Delete hanoi directory

--- hanoi/src/Components/Notes.js
+++
@@ -1,6 +0,0 @@
-import {React} from 'react';
-function Notes() {
-    return <div>Notes</div>;
-}
-
-export default Notes;
--- hanoi/src/Components/Today.js
+++
@@ -1,6 +0,0 @@
-import {React} from 'react';
-function Today() {
-    return <div>Today</div>;
-}
-
-export default Today;
